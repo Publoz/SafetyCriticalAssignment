422dbf725589bfc31a1edf0d8bc7a67e4367032d
Paul Ireland <publoz@Pauls-Air.hub>
2022-03-04 14:59:48 +1300

    Add helper methods that deal with pumps

--- src/steam/boiler/core/MySteamBoilerController.java
+++ src/steam/boiler/core/MySteamBoilerController.java
@@ -63,7 +63,10 @@ public class MySteamBoilerController implements SteamBoilerController {
 	 */
 	@Override
 	public String getStatusMessage() {
-		return mode.toString();
+		String val = mode.toString();
+		assert val instanceof String;
+		return val;
+		
 	}
 
 	/**
@@ -82,10 +85,15 @@ public class MySteamBoilerController implements SteamBoilerController {
 		Message steamMessage = extractOnlyMatch(MessageKind.STEAM_v, incoming);
 		Message[] pumpStateMessages = extractAllMatches(MessageKind.PUMP_STATE_n_b, incoming);
 		Message[] pumpControlStateMessages = extractAllMatches(MessageKind.PUMP_CONTROL_STATE_n_b, incoming);
+		
+		if(levelMessage == null || steamMessage == null) {
+			emergencyMode(outgoing);
+			return;
+		}
 		//
 		if (transmissionFailure(levelMessage, steamMessage, pumpStateMessages, pumpControlStateMessages)) {
 			// Level and steam messages required, so emergency stop.
-			this.mode = State.EMERGENCY_STOP;
+			emergencyMode(outgoing);
 		}
 		//
 
@@ -94,13 +102,62 @@ public class MySteamBoilerController implements SteamBoilerController {
 		case WAITING:
 			doWaiting(incoming, outgoing);
 			outgoing.send(new Message(MessageKind.MODE_m, Mailbox.Mode.INITIALISATION));
+			break;
 		case READY:
 			doReady(incoming, outgoing);
+			break;
+		case NORMAL:
+			doNormal(incoming, outgoing);
+			outgoing.send(new Message(MessageKind.MODE_m, Mailbox.Mode.NORMAL));
+			break;
 			
 		
 		}
 		// NOTE: this is an example message send to illustrate the syntax
 		//outgoing.send(new Message(MessageKind.MODE_m, Mailbox.Mode.INITIALISATION));
+	}
+	
+	public void doNormal(Mailbox incoming, Mailbox outgoing) {
+		Message level = extractOnlyMatch(MessageKind.LEVEL_v, incoming);
+		Message steam = extractOnlyMatch(MessageKind.STEAM_v, incoming);
+		assert level != null && steam != null;
+		int number = calcPumps(level, steam);
+		turnOnPumps(number, outgoing);
+		
+	}
+	
+	public void turnOnPumps(int num, Mailbox outgoing) {
+		for(int i = 0; i < configuration.getNumberOfPumps(); i++) {
+			if(i < num) {
+				outgoing.send(new Message(MessageKind.OPEN_PUMP_n, i));
+			} else {
+				outgoing.send(new Message(MessageKind.CLOSE_PUMP_n, i));
+			}
+		}
+	}
+	
+	public int calcPumps(Message level, Message steam) {
+		
+		int bestNum = 0;
+		double bestDist = 9999;
+		for(int i = 0; i <= configuration.getNumberOfPumps(); i++) {
+//			double last = configuration.getPumpCapacity(0);
+//			for(int j = 1; j < i+1; j++) {
+//				if(last ) //should check capacity is same for all
+//				last = configuration.getPumpCapacity(j);
+//			}
+			
+			double max = level.getDoubleParameter() + (5 * configuration.getPumpCapacity(0) * (i))
+					- (5 * steam.getDoubleParameter());
+			double min = level.getDoubleParameter() + (5 * configuration.getPumpCapacity(0) * (i))
+					- (5 * configuration.getMaximualSteamRate());
+			
+			if(Math.abs(((max + min) / 2) - target) < bestDist) {
+				bestNum = i;
+				bestDist = Math.abs(((max + min) / 2) - target);
+			}
+		}
+		return bestNum;
 	}
 	
 	public void doReady(Mailbox incoming, Mailbox outgoing) {
@@ -110,41 +167,42 @@ public class MySteamBoilerController implements SteamBoilerController {
 		}
 	}
 	
-	public boolean doWaiting(Mailbox incoming, Mailbox outgoing) {
+	public void doWaiting(Mailbox incoming, Mailbox outgoing) {
 		
 		if(extractAllMatches(MessageKind.STEAM_BOILER_WAITING, incoming).length != 1) {
-			return false;
+			return;
 		} 
 		
 		Message levelMessage = extractOnlyMatch(MessageKind.LEVEL_v, incoming);
 		Message steamMessage = extractOnlyMatch(MessageKind.STEAM_v, incoming);
 		if(levelMessage == null || steamMessage == null) {
-			this.mode = State.EMERGENCY_STOP;
-			return false;
+			emergencyMode(outgoing);
 		} else if(steamMessage.getDoubleParameter() != 0){
-			this.mode = State.EMERGENCY_STOP;
-			return false;
+			emergencyMode(outgoing);
 		} else if(levelMessage.getDoubleParameter() < 0 || levelMessage.getDoubleParameter() >= configuration.getCapacity()) { 
 			System.out.println("sensor broken");
-			this.mode = State.EMERGENCY_STOP;
-			return false;
+			emergencyMode(outgoing);
 		} else if(levelMessage.getDoubleParameter() > configuration.getMaximalNormalLevel()
 				|| levelMessage.getDoubleParameter() < configuration.getMinimalNormalLevel()) {
-			hitInitialTarget(levelMessage.getDoubleParameter(), outgoing);
+			hitInitialTarget(levelMessage, steamMessage, outgoing);
 		} else {
 			outgoing.send(new Message(MessageKind.PROGRAM_READY));
 			this.mode = State.READY;
 		}
-		return true;
 		
 	}
+	
+	private void emergencyMode(Mailbox outgoing) {
+		this.mode = State.EMERGENCY_STOP;
+		outgoing.send(new Message(MessageKind.MODE_m, Mailbox.Mode.EMERGENCY_STOP));
+	}
 
-	public void hitInitialTarget(double level, Mailbox outgoing) {
-		if(level > configuration.getMaximalNormalLevel() && !valveOpen) {
+	private void hitInitialTarget(Message level, Message steam, Mailbox outgoing) {
+		if(level.getDoubleParameter() > configuration.getMaximalNormalLevel() && !valveOpen) {
 			outgoing.send(new Message(MessageKind.VALVE));
 			valveOpen = true;
-		} else if(level < configuration.getMinimalNormalLevel()) {
-			outgoing.send(new Message(MessageKind.OPEN_PUMP_n, 0));
+		} else if(level.getDoubleParameter() < configuration.getMinimalNormalLevel()) {
+			turnOnPumps(calcPumps(level, steam), outgoing);
 			if(valveOpen) {
 				outgoing.send(new Message(MessageKind.VALVE));
 				valveOpen = false;
@@ -173,13 +231,7 @@ public class MySteamBoilerController implements SteamBoilerController {
 	private boolean transmissionFailure(Message levelMessage, Message steamMessage, Message[] pumpStates,
 			Message[] pumpControlStates) {
 		// Check level readings
-		if (levelMessage == null) {
-			// Nonsense or missing level reading
-			return true;
-		} else if (steamMessage == null) {
-			// Nonsense or missing steam reading
-			return true;
-		} else if (pumpStates.length != configuration.getNumberOfPumps()) {
+		if (pumpStates.length != configuration.getNumberOfPumps()) {
 			// Nonsense pump state readings
 			return true;
 		} else if (pumpControlStates.length != configuration.getNumberOfPumps()) {
